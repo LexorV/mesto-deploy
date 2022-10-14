fe2358fe375907767d16a0e5f020a45dcb0934e8
Lex <Lexor.V.A.S@yandex.ru>
2022-10-15 02:30:57 +0300

    fix config deploy part_2

--- frontend/ecosystem.config.js
+++ frontend/ecosystem.config.js
@@ -12,9 +12,10 @@ module.exports = {
     production: {
       user: DEPLOY_USER,
       host: DEPLOY_HOST,
+      ssh_options: "StrictHostKeyChecking=no",
       ref: DEPLOY_REF,
       port: DEPLOY_PORT,
-      repo: 'https://github.com/LexorV/web-plus-pm2-deploy.git',
+      repo: 'https://github.com/LexorV/web-plus-pm2-deploy',
       path: DEPLOY_PATH,
       'pre-deploy': `scp -P ${DEPLOY_PORT} .env ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}`,
       'post-deploy': ' cd frontend && npm i && npm run build && cp build/* home/lex/web-project/deployProject/frontend-mesto-deploy',
